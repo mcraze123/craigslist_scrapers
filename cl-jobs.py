877c7fb100518986da536f3d91f4dad26628b780
mcraze123 <mcraze123@gmail.com>
2017-01-07 11:44:02 -0500

    finished porting cl-jobs.pl to python

--- cl-jobs.py
+++ cl-jobs.py
@@ -6,6 +6,8 @@
 
 from time import gmtime, strftime
 import feedparser
+import urllib2
+import re
 
 feeds=[
 'https://knoxville.craigslist.org/search/sof?format=rss',#software/qa/dba
@@ -16,21 +18,40 @@ feeds=[
 'http://knoxville.craigslist.org/search/cpg?format=rss' #computer gigs
 ]
 
-now=strftime("%Y-%m-%d %H:%M:%S", gmtime())
-print now + "\n\n"
+# Get today's date
+today=strftime("%Y-%m-%d", gmtime())
+#print today + "\n"
 
 for f in feeds:
     d=feedparser.parse(f)
-    print "---[[[[" + d.feed.title + "]]]]---\n\n"
+
+    # Print title if there is a job posted today in the feed
+    for post in d.entries:
+        matches=re.search(r"%s" % today,post.date,re.M|re.I)
+        if matches is not None:
+            print "---[[[[" + d.feed.title + "]]]]---\n"
+            break
+
+    # Print the jobs posted today
     for post in d.entries:
         title=post.title.encode('ascii','replace')
         link=post.link.encode('ascii','replace')
         description=post.description.encode('ascii','replace')
-        date=post.date
-        print " " + date + "\n\n"
-        print " " + title + "\n\n"
-        print "\t" + link + "\n\n"
-        print "\t" + description + "\n\n\n"
-        print "========================================================\n\n\n"
-    print "========================================================\n\n\n"
 
+        date=post.date
+        matches=re.search(r"%s" % today,date,re.M|re.I)
+        if matches is not None:
+            print " " + date + "\n"
+            print " " + title + "\n"
+            print "\t" + link + "\n"
+
+            # Get full desctiption
+            response=urllib2.urlopen(link)
+            html=response.read()
+            body_rx=re.compile('<section id="postingbody">(.*?)</section>',re.I|re.M|re.S)
+            matches=body_rx.search(html)
+            description= matches.group(1)
+            description=re.sub(r'<.*?>',"",description)
+            print description + "\n\n" # doesn't print full description
+
+            print "========================================================\n\n"
